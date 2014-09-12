c8e91db3f901e5b4f52880462a3fc55c28a9cb77
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-09-12 17:22:12 +0700

    update js and css

--- js/main.js
+++ js/main.js
@@ -12,34 +12,57 @@ app.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 app.controller('PageCtrl', ['$scope', function ($scope) {
-  $scope.tagline ='Career Analytics. Open Source. Awesome!';
-  $scope.items = ['Career Analytics     Ctrl + Alt + A','Find Job    Ctrl + Alt + F'];
+ 	$scope.tagline ='Career Analytics. Open Source. Awesome!';
+	$scope.items = ['Career Analytics     Ctrl + Alt + A','Find Job    Ctrl + Alt + F'];
 }]);
 
-
-app.run(['$rootScope', function ($rootScope) {
-	$rootScope.settingStyle = function(){
-		var set = $('.setting-content');
-	    set.on('click',function(){
-	      set.find('ul.setting-items').css("display","block");
-	      set.stop().animate({
-	        'width':'125'
-	      });
-	    }).mouseleave(function(){
-	      set.find('ul.setting-items').css("display","none");
-	      $('.keyboard-shortcuts-items').css("display","none");
-	      set.stop().animate({
-	        'width':'28px'
-	      });
-	    });
-	};
-	$rootScope.openKeyboardShortcuts = function(){
-		var key = $('.keyboard');
-		key.on('click', function(){
-			$('.keyboard-shortcuts-items').show( "fade" );
-		});
-	};
-	$rootScope.findjob = function(){
-		alert(1);
-	};
+app.controller('openCompaniesBar', ['$scope', function ($scope) {
+	var cp = $('.companies-bar'),
+	list = $('.companies-list');
+	cp.click(function(){
+		var ic = $(this).find('i');
+		if(ic.hasClass('fa-arrow-up')){
+			ic.removeClass('fa-arrow-up').addClass('fa-arrow-down');
+			$(this).stop().animate({
+				bottom:"120px"
+			});
+			list.stop().animate({
+				height:'120px',
+				display:'block'
+			});
+		}else{
+			ic.removeClass('fa-arrow-down').addClass('fa-arrow-up');
+			$(this).stop().animate({
+				bottom:"0"
+			});
+			list.stop().animate({
+				height:'0',
+				display:'none'
+			});
+		}
+	});
 }])
+
+app.controller('openKeyboardShortcuts', ['$scope', function ($scope) {
+	var key = $('.keyboard');
+	key.on('click', function(){
+		$('.keyboard-shortcuts-items').show( "fade" );
+	});
+}])
+
+app.controller('settingStyle', ['$scope', function ($scope) {
+	var set = $('.setting-content');
+    set.on('click',function(){
+      set.find('ul.setting-items').css("display","block");
+      set.stop().animate({
+        width:'125'
+      });
+    }).mouseleave(function(){
+      set.find('ul.setting-items').css("display","none");
+      $('.keyboard-shortcuts-items').css("display","none");
+      set.stop().animate({
+        width:'28px'
+      });
+    });
+}])
+
